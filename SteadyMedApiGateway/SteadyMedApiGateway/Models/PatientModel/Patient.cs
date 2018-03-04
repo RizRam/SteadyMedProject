8c0272722ce1c96cfd9b10e12e1254311a16ba4c
Craig Rainey <craig.rainey@allyis.com>
2018-03-03 19:51:27 -0800

    Setting up Patient table on the Physician View

--- SteadyMedApiGateway/SteadyMedApiGateway/Models/PatientModel/Patient.cs
+++ SteadyMedApiGateway/SteadyMedApiGateway/Models/PatientModel/Patient.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using SteadyMedApiGateway.Models.MedicationPlan;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace SteadyMedApiGateway.Models.PatientModel
 {
     public class Patient
     {
+        [DisplayName("Paient ID")]
         //Patients ID
         public int ID { get; set; }
 
+        [DisplayName("First Name")]
         //Patients first name
         public string PatientFirstName { get; set; }
 
+        [DisplayName("Last Name")]
         //Patient's last name
         public string PatientLastName { get; set; }
 
+        [DisplayName("Middle Initial")]
         //Patient's middle name
         public string PatientMiddleName { get; set; }
 
+        [DisplayName("Age")]
         //Patient's age
         public string PatientAge { get; set; }
 
--- SteadyMedApiGateway/SteadyMedApiGateway/Models/PhysicianViewModels/PhysicianViewModel.cs
+++ SteadyMedApiGateway/SteadyMedApiGateway/Models/PhysicianViewModels/PhysicianViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SteadyMedApiGateway.Models.PatientModel;
+using System.ComponentModel;
 
 //Model for the physician that will encapsulate information regarding the physician along with
 //references to each patient the physician is responsible for.
@@ -10,12 +11,15 @@ namespace SteadyMedApiGateway.Models.PhysicianViewModels
 {
     public class PhysicianViewModel
     {
+        [DisplayName("ID")]
         //ID of the physician
         public string PhysicianId { get; set; }
 
+        [DisplayName("First Name")]
         //First name of the physician
         public string PhysicianFirstName { get; set; }
 
+        [DisplayName("Last Name")]
         //Last name of the physician
         public string PhysicianLastName { get; set; }
 
