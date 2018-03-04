53bdb5f758f91cb944eca049d835f7b3312ad1ca
Craig Rainey <craig.rainey@allyis.com>
2018-03-03 18:18:57 -0800

    Adding the API Gateway

---
+++ SteadyMedApiGateway/SteadyMedApiGateway/Controllers/MedicationPlanController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using SteadyMedApiGateway.Models.MedicationPlan;
+using System.Diagnostics;
+
+namespace SteadyMedApiGateway.Controllers
+{
+    public class MedicationPlanController : Controller
+    {
+        private HttpClient client = new HttpClient();
+
+        private const string MEDICATION_PLAN_SERVICE_URL = "http://localhost:50151/api/MedPlans/1";
+
+        public async Task<IActionResult> Index(int medicationPlanId)
+        {
+            HttpResponseMessage response = await client.GetAsync(MEDICATION_PLAN_SERVICE_URL);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                MedicationPlan medicationPlan = JsonConvert.DeserializeObject<MedicationPlan>(responseBody);
+                Debug.WriteLine("Medication Patient ID: " + medicationPlan.PatientId);
+                return View(medicationPlan);
+            }
+            return View();
+        }
+    }
+}
---
+++ SteadyMedApiGateway/SteadyMedApiGateway/Models/MedicationPlan/MedicationPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SteadyMedApiGateway.Models.MedicationPlan
+{
+    public class MedicationPlan
+    {
+        public int MedicationPlanId { get; set; }
+        public int PatientId { get; set; }
+        public int PhysicianId { get; set; }
+        public int SteadyMedId { get; set; }
+        public string Medication { get; set; }
+        public int HourlyInterval { get; set; }
+        public int PillsPerInterval { get; set; }
+        public bool Completed { get; set; }
+    }
+}
